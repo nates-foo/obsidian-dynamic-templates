2f6b50de90c2a824c4f174410a413d5bcff4d8bc
Nathanael Silverman <nathanael@hey.com>
2024-02-20 13:25:00 -0500

    Add command that updates templates in the current file

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,29 +10,59 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-interface DynamicTemplate {
-	path: string;
-	lineStart: number;
-	lineEnd?: number;
+class DynamicTemplate {
+	public sourcePath: string;
+	public path: string;
+	public lineStart: number;
+	public lineEnd?: number;
+
+	constructor(sourcePath: string, path: string, lineStart: number) {
+		this.sourcePath = sourcePath;
+		this.path = path;
+		this.lineStart = lineStart;
+	}
+
+	public async generate(app: App): Promise<string | null> {
+		// Based on https://github.com/blacksmithgu/obsidian-dataview/blob/e4a6cab97b628deb22d36b73ce912abca541ad42/src/api/inline-api.ts#L317
+        const viewFile = app.metadataCache.getFirstLinkpathDest(this.path, this.sourcePath);
+		if (!viewFile) return null;
+
+		let contents = await app.vault.cachedRead(viewFile);
+		if (contents.contains("await")) contents = "(async () => { " + contents + " })()";
+
+		const dv = this.getDataviewPlugin(app).api;
+		const current = dv.page(this.sourcePath);
+		// TODO input
+
+		const func = new Function('dv', 'current', 'input', contents);
+		const result = await Promise.resolve(func(dv, current, null));
+		return result;
+	}
+
+	private getDataviewPlugin(app: App): any {
+		// @ts-ignore
+		return app.plugins.plugins['dataview'];
+	}
 }
 
-function getDynamicTemplates(md: string): DynamicTemplate[] {
+function getLines(md: string): string[] {
+	return md.split(/\r?\n/);
+}
+
+function getDynamicTemplates(md: string, sourcePath: string): DynamicTemplate[] {
 	const regexTempStart = /^%%\s+DT:\s+([^%]+)\s+%%\s*$/i;
 	const regexTempEnd = /^%%%%\s*$/;
 
 	const templates: DynamicTemplate[] = [];
 	let currentTemplate: DynamicTemplate | null = null;
 
-	const lines = md.split(/\r?\n/);
+	const lines = getLines(md);
 	for (let i = 0; i < lines.length; i++) {
 		const line = lines[i];
 		const matchStart = line.match(regexTempStart);
 		if (matchStart) {
 			const path = matchStart[1];
-			currentTemplate = {
-				path,
-				lineStart: i
-			}
+			currentTemplate = new DynamicTemplate(sourcePath, path, i);
 			templates.push(currentTemplate);
 
 		} else if (line.match(regexTempEnd) && currentTemplate) {
@@ -53,17 +83,19 @@ export default class MyPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		const { vault } = this.app;
-
-		this.registerEvent(
-			vault.on('modify', async (file) => {
-				if (file instanceof TFile) {
-					const md = await vault.read(file);
-
-					console.log(getDynamicTemplates(md));
+		this.addCommand({
+			id: 'update',
+			name: 'Update',
+			checkCallback: (checking: boolean) => {
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView) {
+					if (!checking && markdownView.file) {
+						this.update(markdownView.file);
+					}
+					return true;
 				}
-			})
-		);;
+			}
+		});
 	}
 
 	onunload() {
@@ -76,6 +108,38 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	public async update(file: TFile) {
+		const { vault } = this.app;
+		const md = await vault.read(file);
+
+		const templates = getDynamicTemplates(md, file.path);
+
+		const lines = getLines(md);
+		let newLines = [];
+
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			newLines.push(line);
+
+			const template = templates.find(t => t.lineStart === i);
+			if (template) {
+				const generatedContent = await template.generate(this.app);
+				if (generatedContent) {
+					newLines.push(...getLines(generatedContent));
+					newLines.push('%%%%');
+				}
+
+				if (template.lineEnd) {
+					i += template.lineEnd - template.lineStart;
+				}
+			}
+		}
+
+		const data = newLines.join("\n");
+		// TODO Add metadata
+		await vault.modify(file, data);
 	}
 }
 
